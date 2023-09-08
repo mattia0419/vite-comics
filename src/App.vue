f9f277292df71bdd464f73ec6579c03a873b1811
mattia0419 <pironemattia6@gmail.com>
2023-09-08 18:43:00 +0200

    add blu cards

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-
+import AppMain from './components/AppMain.vue';
 
 export default {
   components: {
-    AppHeader
+    AppHeader,
+    AppMain
   }
 }
 </script>
@@ -15,7 +16,17 @@ export default {
 <template>
   <header>
     <AppHeader></AppHeader>
+
   </header>
+  <main>
+    <div class="container">
+      <div class="content">
+        <h2> --- Content goes here --- </h2>
+      </div>
+    </div>
+
+    <AppMain></AppMain>
+  </main>
 </template>
 
 
@@ -23,11 +34,31 @@ export default {
 
 <style lang="scss">
 @use './assets/general.scss';
+@use './assets/partial/_mixins.scss' as *;
+@use './assets/partial/_variables.scss' as *;
 @import url('https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap');
 
 
 body {
   background-color: black;
   font-family: 'Fira Sans', sans-serif;
+}
+
+.content {
+  width: 100%;
+  height: 13vh;
+  @include center('vertically');
+
+  h2 {
+    color: $color_secondary;
+  }
+}
+
+.container {
+  @include center('orizzontaly');
+  width: 1000px;
+  height: 100%;
+  margin: auto;
+
 }
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,88 @@
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                {
+                    image: 'buy-comics-digital-comics.png',
+                    description: 'DIGITAL COMICS'
+                },
+                {
+                    image: 'buy-comics-merchandise.png',
+                    description: 'DC MERCHANDISE'
+                },
+                {
+                    image: 'buy-comics-subscriptions.png',
+                    description: 'SUBSCRIPTION'
+                },
+                {
+                    image: 'buy-comics-shop-locator.png',
+                    description: 'COMIC SHOP LOCATOR'
+                },
+                {
+                    image: 'buy-dc-power-visa.svg',
+                    description: 'DC POWER VISA'
+                },
+
+            ]
+        }
+    },
+    methods: {
+        buildingImagePath(imageName) {
+            const imageURL = new URL('../assets/img/' + imageName, import.meta.url);
+            console.log(imageURL);
+
+            return imageURL.href;
+        }
+    }
+
+}
+</script>
+
+<template>
+    <div class="blu-content">
+        <div class="container">
+            <div class="card" v-for="item in items">
+                <img :src="buildingImagePath(item.image)" alt="">
+                <span>{{ item.description }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../assets/general.scss';
+@use '../assets/partial/_variables.scss' as *;
+@use '../assets/partial/_mixins.scss' as *;
+
+.container {
+    @include center('orizzontaly');
+    width: 1000px;
+    height: 100%;
+    margin: auto;
+}
+
+.blu-content {
+    width: 100vw;
+    height: 20vh;
+    background-color: $color_primary;
+}
+
+.card {
+    height: 100%;
+    width: 200px;
+    cursor: pointer;
+    @include center();
+
+    img {
+        width: 35px;
+    }
+
+    span {
+        color: $color_secondary;
+        font-size: 15px;
+        margin-left: 10px;
+    }
+}
+</style>
