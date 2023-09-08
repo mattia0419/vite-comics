6619540d9db907a9e55df62fc489399bf655b668
mattia0419 <pironemattia6@gmail.com>
2023-09-08 17:47:26 +0200

    add header

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+
+
+
+<template>
+  <header>
+    <AppHeader></AppHeader>
+  </header>
+</template>
+
+
+
+
+<style lang="scss">
+@use './assets/general.scss';
+
+body {
+  background-color: black;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,87 @@
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                'CHARACTERS',
+                'COMICS',
+                'MOVIES',
+                'TV',
+                'GAMES',
+                'COLLECTIBLES',
+                'VIDEOS',
+                'FANS',
+                'NEWS',
+                'SHOP'
+            ]
+        };
+    }
+}
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="/img/dc-logo.png" alt="">
+            </div>
+            <div v-for="link in links" class="link-list">
+                <a href="#">{{ link }}</a>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/general.scss';
+@use '../assets/partial/_variables.scss' as *;
+@use '../assets/partial/_mixins.scss' as *;
+
+header {
+    background-color: $color_secondary;
+    height: 13vh;
+    margin-top: 20px;
+}
+
+.container {
+    @include center();
+    width: 1000px;
+    height: 100%;
+    margin: auto;
+
+}
+
+.logo {
+    height: 100%;
+    width: 50%;
+    @include center('vertically');
+    justify-content: flex-start;
+
+    img {
+        width: 80px;
+    }
+
+
+}
+
+.link-list {
+    margin-left: 20px;
+    font-size: 12px;
+    height: 100%;
+    cursor: pointer;
+    @include center('vertically');
+
+    a {
+        text-decoration: none;
+        color: black;
+    }
+}
+
+.link-list:hover {
+    border-bottom: solid 5px $color_primary;
+
+    a {
+        color: $color_primary;
+    }
+}
+</style>
